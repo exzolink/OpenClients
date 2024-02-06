f6e02f4ef06b03a35f26a1bb8b2671d4f88e931b
exzolink <exzolink@gmail.com>
2024-02-06 03:45:10 +0300

    Init folders and base settings

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const { locale } = useI18n();
+</script>
+
+<template>
+  <form>
+    <select v-model="locale">
+      <option value="en">English</option>
+      <option value="ru">Русский</option>
+    </select>
+    <p>{{ $t('welcome') }}</p>
+  </form>
+</template>
+
+<style scoped>
+
+</style>
